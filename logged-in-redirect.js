74b5ac4b80f95c1ab89b3155354143645cc7baa5
DevSquashLevels <136070804+DevSquashLevels@users.noreply.github.com>
2024-12-10 10:31:36 +0200

    Create logged-in-redirect.js

---
+++ logged-in-redirect.js
@@ -0,0 +1,28 @@
+<script>
+	function getCookieValue(name) {
+  const regex = new RegExp(`(^| )${name}=([^;]+)`);
+  const match = document.cookie.match(regex);
+  if (match) {
+    return decodeURIComponent(match[2]);
+  }
+}
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    if (window.location.pathname === '/' ) {
+			//get SquashlevelsJwt from cookie
+      const jwt = getCookieValue('SquashlevelsJWT');
+      if (jwt) {
+        const domain = window.location.hostname;
+
+        //decode jwt
+        const jwtPayload = JSON.parse(atob(jwt.split('.')[1]));
+
+        //check if jwt domain matches current domain
+
+        if (jwtPayload.iss === domain) {
+          window.location.href = "https://app." + domain; // Redirect to app subdomain
+        }
+      }
+    }
+});
+</script>
