c95a79b738391c0833c59e16ba58514c5e960c37
DevSquashLevels <136070804+DevSquashLevels@users.noreply.github.com>
2024-12-10 10:43:42 +0200

    Update logged-in-redirect.js

--- logged-in-redirect.js
+++ logged-in-redirect.js
@@ -1,28 +1,26 @@
-<script>
-	function getCookieValue(name) {
-  const regex = new RegExp(`(^| )${name}=([^;]+)`);
-  const match = document.cookie.match(regex);
-  if (match) {
-    return decodeURIComponent(match[2]);
-  }
+function getCookieValue(name) {
+const regex = new RegExp(`(^| )${name}=([^;]+)`);
+const match = document.cookie.match(regex);
+if (match) {
+return decodeURIComponent(match[2]);
 }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    if (window.location.pathname === '/' ) {
-			//get SquashlevelsJwt from cookie
-      const jwt = getCookieValue('SquashlevelsJWT');
-      if (jwt) {
-        const domain = window.location.hostname;
+}
 
-        //decode jwt
-        const jwtPayload = JSON.parse(atob(jwt.split('.')[1]));
+document.addEventListener('DOMContentLoaded', () => {
+if (window.location.pathname === '/' ) {
+//get SquashlevelsJwt from cookie
+const jwt = getCookieValue('SquashlevelsJWT');
+if (jwt) {
+const domain = window.location.hostname;
 
-        //check if jwt domain matches current domain
+//decode jwt
+const jwtPayload = JSON.parse(atob(jwt.split('.')[1]));
 
-        if (jwtPayload.iss === domain) {
-          window.location.href = "https://app." + domain; // Redirect to app subdomain
-        }
-      }
-    }
+//check if jwt domain matches current domain
+
+if (jwtPayload.iss === domain) {
+window.location.href = "https://app." + domain; // Redirect to app subdomain
+}
+}
+}
 });
-</script>
